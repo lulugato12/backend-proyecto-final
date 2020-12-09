51577d3fa0172f277c523ae78efb13bacbecbe51
Lulu <A01732177@itesm.mx>
2020-12-09 17:59:20 -0600

    falta debuguear y hacer delete

--- index.js
+++ index.js
@@ -6,14 +6,46 @@ const port = 3000;
 var admin = require("firebase-admin");
 var serviceAccount = require("./proyecto-final-5597c-firebase-adminsdk-6isyj-202cb58a3b.json");
 
+/* credenciales */
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
+/* middlewares */
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+/* objeto db */
 const db = admin.firestore();
+let collection = db.collection('posts')
 
+/* endpoint de post*/
 
-/* listenes */
+/* post */
+app.post('/post', async (req, res) => {
+  const {usuario, descripcion} = req.body;
+  const post = {usuario, descripcion};
+  await collection.doc().set(post)
+  .then(respuesta => res.status(201).send(respuesta))
+  .catch(err => console.log(err))
+})
+
+/* get */
+app.get('/users', async (req, res) => {
+  const posts = [];
+  try{
+    const snapshot = await collection.get();
+    snapshot.forEach(doc => users.push({id: doc.id, data: doc.data()}));
+    res.status(200).send(users);
+  }
+  catch(error){
+    console.log(error);
+  }
+})
+
+/* delete */
+
+/* listener */
 app.listen(port, () => {
   console.log('example running...')
 })
