2976d5c1ab257e59bea5640e4b78ee2ad7f6267f
lulu <A01732177@itesm.mx>
2020-12-10 21:56:05 -0600

    desrdtfgh

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
 
 const randomWords = require('random-words');
 const request = require('request');
