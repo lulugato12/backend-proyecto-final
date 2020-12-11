67c33c2d05db40c1d9e74bf2dd6fee925d7fe92e
lulu <A01732177@itesm.mx>
2020-12-10 21:48:39 -0600

    las cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -22,6 +23,7 @@ admin.initializeApp({
 });
 
 /* middlewares */
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
